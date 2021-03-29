153f013f7df252e66b2acfb5f1161261503ed311
Mehmet Alici <calientesea@gmail.com>
2021-03-30 00:54:18 +0200

    complete until todo-3

---
+++ trials/try.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    char a, b;
+    cin >> a >> b;
+
+    cout << a << endl;
+    cout << b << endl;
+
+
+    return EXIT_SUCCESS;
+}
